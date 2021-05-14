a3671009941c179fbf9f60d4fe593ecddcf731c0
hsshin94 <hsshin9406@gmail.com>
2021-05-14 20:53:28 +0900

    충돌이 두렵다ㅠ

--- First-Project/src/first/HelloWorld.java
+++ First-Project/src/first/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello,World");
 		System.out.println("my name is ulululul");
 		System.out.println("my name is naul");
+		System.out.println("面倒篮 公辑款单ばば");
 	}
 
 }
